c90b65c78b9bfa65194cefd15766a59a43aacb8e
Admin <vuminhduc24081997@gmail.com>
2022-12-25 11:31:29 +0700

    duc 2408 commit 7 sang duc2408 car 3 45

--- src/Car.java
+++ src/Car.java
@@ -2,5 +2,6 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("car dep  3");
         System.out.println(" car dep 4");
+        System.out.println(" car dep 5");
     }
 }
